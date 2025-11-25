21c5b4c0ccf7cd7d19db707af5b8160497df1865
Derya Atmaca <deryaatmc@gmail.com>
2025-11-25 13:51:13 +0300

    Initial commit: While ve Foreach döngü örnekleri

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While Döngüsü
+            // 1'den başlayarak console'dan girilen sayıya kadar olan sayıların ortalamasını hesaplayalım.
+
+            Console.Write("Lütfen bir sayı giriniz: ");
+            if (!int.TryParse(Console.ReadLine(), out int sayi) || sayi <= 0)
+            {
+                Console.WriteLine("Geçerli bir sayı giriniz!");
+                return;
+            }
+            int sayac = 1;
+            int toplam = 0;
+
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+
+            double ortalama = (double)toplam / sayi;
+            Console.WriteLine("Ortalama: " + ortalama);
+
+            // Foreach Döngüsü
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+            Console.WriteLine("\nProgram sona erdi.");
+        }
+    }
+}
